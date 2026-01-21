ca1c91d38680e20610df543c32743b0626074f1f
0x24a <miku399993@gmail.com>
2026-01-21 14:15:42 +0800

    deps: bump version to v1.0.0 and remove .python-version limit

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from tempfile import mkdtemp
 from typer import run
 from time import time
 
-__VERSION__ = "0.1.0"
+__VERSION__ = "1.0.0"
 
 def check_ffmpeg(ffmpeg_path: str = "ffmpeg") -> bool:
     try:
