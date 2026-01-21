9918646be4259fbd6e0b98a13374942c8d3dadb6
0x24a <miku399993@gmail.com>
2026-01-21 14:02:08 +0800

    feat: better progress identicator

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ def main(
     except ValueError:
         print("[red]ˣ  Invalid key format. It should be a hexadecimal string.[/red]")
         exit(1)
-    print("[yellow](1)[/yellow] Checking FFMpeg... ", end="")
+    print("[yellow](1/7)[/yellow] Checking FFMpeg... ", end="")
     if check_ffmpeg(ffmpeg_path):
         print("[green]OK[/green]")
     else:
@@ -79,7 +79,7 @@ def main(
         print("[red]   Please install FFMpeg from https://ffmpeg.org/download.html[/red]")
         print("[red]   Or specify the path to the executable using the --ffmpeg-path option.[/red]")
         exit(1)
-    print("[yellow](2)[/yellow] Checking FFProbe... ", end="")
+    print("[yellow](2/7)[/yellow] Checking FFProbe... ", end="")
     if check_ffprobe(ffprobe_path):
         print("[green]OK[/green]")
     else:
@@ -88,11 +88,11 @@ def main(
         print("[red]   Please install FFProbe from https://ffmpeg.org/download.html[/red]")
         print("[red]   Or specify the path to the executable using the --ffprobe-path option.[/red]")
         exit(1)
-    print("[yellow](3)[/yellow] Copying file to the temporary folder... ", end="")
+    print("[yellow](3/7)[/yellow] Copying file to the temporary folder... ", end="")
     temp_folder = mkdtemp()
     shutil.copy(source, path.join(temp_folder, path.basename(source)))
     print("[green]OK[/green]")
-    print("[yellow](4)[/yellow] Converting the source file to VP9 (IVF) encoding...")
+    print("[yellow](4/7)[/yellow] Converting the source file to VP9 (IVF) encoding...")
     task = FFMpegTask(
         ffmpeg_path,
         "-i", path.join(temp_folder, path.basename(source)),
@@ -115,20 +115,20 @@ def main(
         else:
             live.update(f"-  [green]Finished in {end_time - start_time:.2f} seconds[/green]")
     with Live(Text(""), refresh_per_second=16) as live:
-        live.update("[bold][yellow](5)[/yellow][/bold] Converting to USM (.dat)... Pending")
+        live.update("[bold][yellow](5/7)[/yellow][/bold] Converting to USM (.dat)... Pending")
         video = Vp9(path.join(temp_folder, "source.ivf"), ffprobe_path=ffprobe_path) # pyright: ignore[reportAbstractUsage]
         usm = Usm(videos=[video], audios=None, key=int(key, 16), version=usm_version)
         chunk_n = 1
         with open(path.join(temp_folder, "target.usm"), "wb") as f:
             for chunk in usm.stream(OpMode.ENCRYPT, encoding="shift-jis"):
-                live.update(f"[bold][yellow](5)[/yellow][/bold] Converting to USM (.dat)... Chunk {chunk_n}")
+                live.update(f"[bold][yellow](5/7)[/yellow][/bold] Converting to USM (.dat)... Chunk {chunk_n}")
                 chunk_n += 1
                 f.write(chunk)
-        live.update("[bold][yellow](5)[/yellow][/bold] Converting to USM (.dat)... [green]Done[/green]")
-    print("[yellow](6)[/yellow] Copying back the result... ", end="")
+        live.update("[bold][yellow](5/7)[/yellow][/bold] Converting to USM (.dat)... [green]Done[/green]")
+    print("[yellow](6/7)[/yellow] Copying back the result... ", end="")
     shutil.copy(path.join(temp_folder, "target.usm"), destination)
     print("[green]OK[/green]")
-    print("[yellow](7)[/yellow] Cleaning up... ", end="")
+    print("[yellow](7/7)[/yellow] Cleaning up... ", end="")
     shutil.rmtree(temp_folder)
     print("[green]OK[/green]")
     print(f"[green]Successfully converted {source} to {destination} with key {key} and USM version {usm_version}.[/green]")
